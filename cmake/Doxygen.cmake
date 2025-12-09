da67ba325eb9cb0a4a93073cb2aadbd64747d125
Sammyalhashe <sammy@salh.xyz>
2025-12-09 01:16:41 -0500

    Fix Doxygen find_package issues and update deploy-docs workflow

--- cmake/Doxygen.cmake
+++ cmake/Doxygen.cmake
@@ -3,12 +3,13 @@ find_package(Graphviz REQUIRED) # Make Graphviz finding mandatory
 
 # Define variables for configure_file in this scope
 set(DOXYGEN_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/doc_doxygen")
+# GRAPHVIZ_DOT_EXECUTABLE will be provided by find_package(Graphviz REQUIRED)
 set(GRAPHVIZ_DOT_EXECUTABLE_PATH "${GRAPHVIZ_DOT_EXECUTABLE}")
 
 
-if(DOXYGEN_FOUND AND GRAPHVIZ_FOUND)
+if(DOXYGEN_FOUND AND Graphviz_FOUND) # Corrected to Graphviz_FOUND
     # Removed debug messages: message(STATUS "Doxygen found: ${DOXYGEN_FOUND}, Executable: ${DOXYGEN_EXECUTABLE}")
-    # Removed debug messages: message(STATUS "Graphviz found: ${GRAPHVIZ_FOUND}, dot Executable: ${GRAPHVIZ_DOT_EXECUTABLE}")
+    # Removed debug messages: message(STATUS "Graphviz found: ${Graphviz_FOUND}, dot Executable: ${GRAPHVIZ_DOT_EXECUTABLE}")
 
     set(DOXYGEN_IN ${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile.in)
     set(DOXYGEN_OUT ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
