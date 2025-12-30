93308995ab4f8ed6dc1a84ae0e8ed22ae04ee3e9
Sammyalhashe <sammy@salh.xyz>
2025-12-30 16:40:55 -0500

    build: modularize static analysis targets and fix doxygen config

--- cmake/StaticAnalyzers.cmake
+++ cmake/StaticAnalyzers.cmake
@@ -8,3 +8,16 @@ if (ENABLE_CLANG_TIDY)
         message(SEND_ERROR "clang-tidy requested but executable not found")
     endif()
 endif()
+
+add_custom_target(cppcheck
+    COMMAND cppcheck --enable=warning,style,performance,portability --suppress=missingIncludeSystem --suppress=unusedFunction --suppress=knownConditionTrueFalse --suppress=virtualCallInConstructor --suppress=noExplicitConstructor --suppress=missingOverride --suppress=shadowArgument --suppress=useStlAlgorithm --suppress=dangerousTypeCast --suppress=variableScope --suppress=unreadVariable --suppress=noConstructor --suppress=cstyleCast --suppress=unusedVariable --suppress=normalCheckLevelMaxBranches --std=c++20 --quiet --error-exitcode=1 ${CMAKE_SOURCE_DIR}/adaptors ${CMAKE_SOURCE_DIR}/common ${CMAKE_SOURCE_DIR}/databases ${CMAKE_SOURCE_DIR}/executors ${CMAKE_SOURCE_DIR}/protocols ${CMAKE_SOURCE_DIR}/strategies ${CMAKE_SOURCE_DIR}/traders
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    COMMENT "Running cppcheck..."
+)
+
+add_custom_target(style-check
+    COMMAND python3 ${CMAKE_SOURCE_DIR}/style_checker.py adaptors common databases executors protocols strategies traders
+    DEPENDS cppcheck
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    COMMENT "Running style checker..."
+)
